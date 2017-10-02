b41575ff5b1938d61a5f4a9a5a3302ee88c5b230
William Breen <wbreen@carpenter-03.elon.edu>
2017-10-01 21:15:15 -0400

    downloaded the correct searching files

--- SearchingHomework/searches/iterastar.pl
+++ SearchingHomework/searches/iterastar.pl
@@ -10,33 +10,34 @@ countNodes(0).
 go(Start, Goal):-
 	retract(countNodes(X)),
 	asserta(countNodes(0)),
-	astar_limited(Start, Goal, 1).
+	heuristic(Start,Goal,H),
+	astar_limited(Start, Goal, H).
 
 
 
-astar_limited(Start,Goal, MaxDepth):-
+astar_limited(Start,Goal, MaxF):-
 	empty_pq(Open),
 	empty_set(Closed_set),
 	heuristic(Start,Goal,H),
 	insert_pq([Start,nil,0,H,H,1],Open,Open_pq),
-	path(Open_pq,Closed_set,Start,Goal, MaxDepth).
+	max_heuristic(Max),
+	path(Open_pq,Closed_set,Start,Goal, MaxF, Max).
 
 
 % case of empty open stack - no path found.
 
-path(Open,_,Start,Goal,MaxDepth):-
+path(Open,_,Start,Goal,MaxF,NewMax):-
 	empty_pq(Open),
 	countNodes(X),
 	writelist([X,' nodes visited.']),
-	write('At '), write(MaxDepth),
-	write(' Graph searched, no solution found.'),nl,
-	NewMax is MaxDepth + 1,!,
+	write('At heuristic max : '), write(MaxF),
+	write(' Graph searched, no solution found.'),nl,!,
 	astar_limited(Start, Goal,NewMax).
 	
 
 % case of goal found on open stack, print path.
 
-path(Open_pq,Closed_set,_,Goal, _):-
+path(Open_pq,Closed_set,_,Goal, _,_):-
 	
 	dequeue_pq([State,Parent, _,_,_,Depth],Open_pq,_),
 	State = Goal,
@@ -50,7 +51,7 @@ path(Open_pq,Closed_set,_,Goal, _):-
 % general case - put the children of current state on open list,
 % put state on closed list
 	
-path(Open_pq,Closed_set,Start,Goal, MaxDepth):-
+path(Open_pq,Closed_set,Start,Goal, MaxF,NewMax):-
 	dequeue_pq([State,Parent, D, H, S, Depth], Open_pq,Rest_open_pq),
 	retract(countNodes(X)),		% update # nodes visited
 	Y is X +1,
@@ -59,21 +60,28 @@ path(Open_pq,Closed_set,Start,Goal, MaxDepth):-
 	write('Current state is: '),
 	write(State),nl,
 	write('Expanding New Nodes.'),nl,
-	get_children([State, Parent, D, H, S, Depth],Rest_open_pq,Closed_set,Children,Goal,MaxDepth),
+	get_children([State, Parent, D, H, S, Depth],Rest_open_pq,Closed_set,Children,Goal,MaxF,MinExceeding),
 	insert_list_pq(Children,Rest_open_pq,New_open_pq),
 	add_if_not_in_set([State,Parent,D,H,S,Depth],Closed_set,New_closed_set),
-	path(New_open_pq,New_closed_set,Start,Goal,MaxDepth),!.
+	((MinExceeding < NewMax, NextMax = MinExceeding); NextMax = NewMax),
+	path(New_open_pq,New_closed_set,Start,Goal,MaxF,NextMax),!.
 
 
 % General predicates and ADT specifications:
 
 % Gets children of State
 
-get_children([_,_,_,_,_,Depth],_,_,Children,_,MaxDepth):- MaxDepth = Depth, Children = [].
+get_children([_,_,_,_,F,_],_,_,Children,_,MaxF,F):-F > MaxF , Children = [].
 
-get_children([State,_,D,_,_,Depth],Rest_open_pq,Closed_set,Children,Goal,MaxDepth):- MaxDepth > Depth,
-	(bagof(Child,moves([State,_,D,_,_,Depth],Rest_open_pq,Closed_set,Child,Goal),Children);Children = []).
+get_children([State,_,D,_,F,StateDepth],Rest_open_pq,Closed_set,Children,Goal,MaxF,MinExceeding):-
+   MaxF >= F,
+	(bagof(Child,moves([State,_,D,_,F,StateDepth],Rest_open_pq,Closed_set,Child,Goal),Children);Children = []),
+	findMinExceeding(Children,MaxF,MinExceeding).
 
+findMinExceeding([],_,Max):-max_heuristic(Max).
+findMinExceeding([H|T],MaxF,Max):-H = [_,_,_,_,Heur,_], Heur > MaxF, !, findMinExceeding(T,MaxF,TailMax), 
+		((TailMax<Heur, Max = TailMax); Max = Heur).
+findMinExceeding([H|T],MaxF,Max):-H = [_,_,_,_,Heur,_], Heur =< MaxF, !, findMinExceeding(T,MaxF,Max). 	
 
 % Finds all legal moves from State
 
@@ -104,6 +112,9 @@ insert_list_pq([],L,L).
 insert_list_pq([State|Tail],L,New_L):-
 	insert_pq(State,L,L2),
 	insert_list_pq(Tail,L2,New_L).
+	
+print_pq([]):-nl.
+print_pq([H|T]):- write(H), write(','), print_pq(T).
 
 member_set(E,S):- member(E,S).
 
@@ -126,3 +137,5 @@ writelist([]).
 writelist([H|T]):- write(H),  writelist(T).
 
 precedes([_,_,_,_,S1,_],[_,_,_,_,S2,_]) :- S1 < S2.
+
+max_heuristic(99999).
