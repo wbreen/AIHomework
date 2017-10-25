8d0a5b62057a611178ce258e6b7304bd7571e451
William Breen <wbreen@duke303-30.elon.edu>
2017-10-25 15:22:52 -0400

    started making the decision tree, need to finish first elif statement

--- ID3TreeLearningStudent/src/id3/ID3.java
+++ ID3TreeLearningStudent/src/id3/ID3.java
@@ -1,6 +1,6 @@
 package id3;
 /*
- * Author:
+ * Author: William Breen
  * ID3 Algorithm Implemented
  */
 import java.util.*;
@@ -14,6 +14,35 @@ public class ID3 {
 	// the decision, and a default Decision.  
 	// It returns the decision tree.
 	public TreeNode id3(List<Example> examples, List<Attribute> attributes, Decision defaultDecision){
+		//if examples is empty, then return default
+		if(examples.isEmpty()) {
+			return defaultDecision;
+		}
+		
+		//else if all examples have the same classification, then return the classification
+		//TODO: finish this elif statement
+		else if(areAllSame(examples)) {
+			//needs to change, this is just a temporary value
+			return defaultDecision;
+		}
+		//else if attributes is empty, then return Majority-Value(examples)
+		
+		//else (see pseudocode)
+		
 		return defaultDecision;
+	}
+	
+	//returns if the given list of examples all have the same 
+	public boolean areAllSame(List<Example> myList) {
+		int len = myList.size();
+		Example compareEx = myList.get(0);
+		for(int i = 0; i<len; i++) {
+			Example currentEx = myList.get(i);
+			if(!compareEx.equals(currentEx)) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 }
