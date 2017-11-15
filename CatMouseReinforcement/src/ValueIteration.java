b259bf70411a477b40976b1f7e5711f917d976c3
William Breen <wbreen@duke303-30.elon.edu>
2017-11-15 15:04:35 -0500

    worked on catMouse and removed big png files from genetic algos because theyre in google drive

--- CatMouseReinforcement/src/ValueIteration.java
+++ CatMouseReinforcement/src/ValueIteration.java
@@ -9,24 +9,64 @@
  */
 public class ValueIteration {
 
-    /** the computed optimal policy for the given MDP **/
-    public int policy[];
+	/** the computed optimal policy for the given MDP **/
+	public int policy[];
 
-    /** the computed optimal utility for the given MDP **/
-    public double utility[];
+	/** the computed optimal utility for the given MDP **/
+	public double utility[];
 
-    /** the precision used to determine when to stop iterating (called
-     * epsilon in lecture) **/
-    public static double precision = 1e-10;
+	/** the precision used to determine when to stop iterating (called
+	 * epsilon in lecture) **/
+	public static double precision = 1e-10;
 
-    /**
-     * The constructor for this class.  Computes the optimal policy
-     * for the given <tt>mdp</tt> with given <tt>discount</tt> factor,
-     * and stores the answer in <tt>policy</tt>.  Also stores the
-     * optimal utility in <tt>utility</tt>.
-     */
-    public ValueIteration(Mdp mdp, double discount) {
-    		
-    }
+	/**
+	 * The constructor for this class.  Computes the optimal policy
+	 * for the given <tt>mdp</tt> with given <tt>discount</tt> factor,
+	 * and stores the answer in <tt>policy</tt>.  Also stores the
+	 * optimal utility in <tt>utility</tt>.
+	 */
+	public ValueIteration(Mdp mdp, double discount) {
+		//vector of utilities for mdp states in S
+		double utility = 0;
+		double uPrime = 0;
+		//max change in utility of any state in an iteration
+		double delta = 0;
+		double compareDiscount = precision*(1-discount)/discount;
+
+		while(delta >= compareDiscount) {
+			utility = uPrime;
+			delta = 0;
+			for(int i = 0; i < mdp.stateName.length; i++) {
+				
+				uPrime = BellEquation(mdp.reward[i], discount);
+				if(uPrime - utility > delta) {
+					delta = uPrime-utility;
+				}
+			}
+
+		}
+	}
+
+	//this is the value after the sigma in the Bellman Equation
+	public double expectedUtility(int state, int action, Mdp mdp) {
+		double expectedValue = 0;
+		//list of all possible next states after taking this action
+		int[] nextStates = mdp.nextState[state][action];
+		for(int i = 0; i <nextStates.length; i++) {
+			int nextState = nextStates[i];
+			double prob = mdp.transProb[state][action][i];
+			double util = utility[nextState];
+			expectedValue += prob*util;
+		}
+		return expectedValue;
+	}
+
+	//returns the value of the new vector
+	public double BellEquation(double reward, double disc) {
+		double vector = 0;
+
+
+		return vector;
+	}
 
 }
